3800c60b8526c6970c9a6fe4c18c6187bd561d1b
srwa.farkha@hotmail.com <srwa.farkha@hotmail.com>
2023-02-02 16:08:46 +0100

    working with car list

--- scripts/site.js
+++ scripts/site.js
@@ -1,36 +1,63 @@
-const productList = document.querySelector('.product-list');
+'use strict';
 
-eventListeners();
+const gallery = document.querySelector('.gallery');
+const modalDialog = document.querySelector('.modal-car-details');
+const closeModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
 
-// all event listeners
-function eventListeners(){
-    window.addEventListener('DOMContentLoaded', () => {
-        loadJSON();
-    });
 
-function loadJSON(){
-    fetch('./data/cars.json')
-    .then(response => response.json())
-    .then(data =>{
-        let html = '';
-        data.forEach(product => {
-            html += `
-                <div class = "product-item">
-                    <div class = "card">
-                        <img class= "card-img-top" src = "${product.img}" alt = "product image">
-        
-                    </div>
-                    <div class = "card-body">
-                        <h3 class = "text">${product.gear}</h3>
-                        <span class = "product-price">Pris: ${product.price}</span>
-                        <p class = "product-price">${product.price} kr</p>
-                    </div>
-                </div>
-            `;
-        });
-        productList.innerHTML = html;
-    })
-    .catch(error => {
-        alert(`ERROR`);
-    })}
+vehicles.forEach(car => {
+  gallery.insertAdjacentHTML(
+    'beforeend',
+    `<div class="car">
+    <img id="${car.id}" src="./content/img/${car.img}"/>
+    <p>${car.milage} ${car.price}</p>
+    </div>`
+  );
+});
+
+const images = document.querySelectorAll('.car img');
+
+const openModal = function(id) {
+  const image = modalDialog.querySelector('.modal-container');
+
+  var selectedCar = vehicles.find(x => x.id == id);
+  console.log(selectedCar)
+
+  image.innerHTML = `
+    <div class="card"">
+    <img class="card-img-top" src="./content/img/${selectedCar.img}">
+    <div class="card-body">
+        <h5 class="card-title">${selectedCar.brand}</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+    `;
+
+  overlay.classList.remove('hidden');
+  modalDialog.classList.remove('hidden');
 }
+
+for(let image of images){
+  let id = image.getAttribute('id');
+
+  image.addEventListener('click', function() {
+    openModal(id);
+  });
+}
+
+const quitModal = () => {
+  modalDialog.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+closeModal.addEventListener('click', quitModal);
+
+overlay.addEventListener('click', quitModal);
+
+document.addEventListener('keydown', function(e){
+  if(e.key === 'Escape') {
+    if(!modalDialog.classList.contains('hidden')){
+      quitModal();
+    }
+  }
+})
--- scripts/vehicles.js
+++ scripts/vehicles.js
@@ -1,62 +1,62 @@
-[
+const vehicles = [
     {
         "id": 1,
-        "car-brand": "Audi",
+        "brand": "Audi",
         "model": "A6",
         "colour": "Svart",
         "year": 2022,
         "price": 400000,
         "milage": 6500,
         "gear": "Automat",
-        "img": "./content/img/javascript_logo.png"
+        "img": "audi-A6-2022.png"
         
     },
     {
         "id": 2,
-        "car-brand": "BMW",
+        "brand": "BMW",
         "model": "520i",
         "colour": "Mörk blå",
         "year": 2020,
         "price": 300000,
         "milage": 8580,
         "gear": "Manuell",
-        "img": "./content/img/javascript_logo.png"
+        "img": "bmw-520i-2020.png"
         
     },
     {
         "id": 3,
-        "car-brand": "Mercedes Benz",
+        "brand": "Mercedes Benz",
         "model": "C220",
         "colour": "Vit",
         "year": 2022,
         "price": 350000,
         "milage": 3000,
         "gear": "Automat",
-        "img": "./content/img/javascript_logo.png"
+        "img": "mercedes-benz-2022-c220.png"
         
     },
     {
         "id": 4,
-        "car-brand": "Volvo",
+        "brand": "Volvo",
         "model": "XC90",
         "colour": "Svart",
         "year": 2019,
         "price": 280000,
         "milage": 9500,
         "gear": "Automat",
-        "img": "./content/img/javascript_logo.png"
+        "img": "Volvo-XC90-2019.png"
         
     },
     {
         "id": 5,
-        "car-brand": "Mercedes Benz",
-        "model": "E300",
+        "brand": "Mercedes Benz",
+        "model": "E450",
         "colour": "Svart",
         "year": 2023,
         "price": 500000,
         "milage": 1500,
         "gear": "Automat",
-        "img": "./content/img/javascript_logo.png"
+        "img": "mercedes-benz-e450-2023.png"
         
     }
 ]
